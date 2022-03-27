9478b5e3a1c2c01bfdf2c56553d0f19cc4359f05
Oscar <oscarla5747@gmail.com>
2022-03-27 15:17:14 -0700

    Edit dataframe columns

--- helpers/model_builders.py
+++ helpers/model_builders.py
@@ -15,9 +15,12 @@ def build_dataframe(player_id, player_data, opponent_id):
     df['totalMatchesWon'] = (df['winnerId'] == player_id).cumsum().sub(df['winnerId'] == player_id)
     df['totalMatchesLost'] = (df['loserId'] == player_id).cumsum().sub(df['loserId'] == player_id)
 
-    df['totalPointsWon'] = np.where(df['winnerId'] == player_id, df['winnerPoints'], df['loserPoints']).cumsum()
+    df['pointsWon'] = np.where(df['winnerId'] == player_id, df['winnerPoints'], df['loserPoints'])
+    df['pointsLost'] = np.where(df['winnerId'] == player_id, df['loserPoints'], df['winnerPoints'])
+
+    df['totalPointsWon'] = df['pointsWon'].cumsum()
     df['totalPointsWon'] = np.where(df['winnerId'] == player_id, df['totalPointsWon'].sub(df['winnerPoints']), df['totalPointsWon'].sub(df['loserPoints']))
-    df['totalPointsLost'] = np.where(df['winnerId'] == player_id, df['loserPoints'], df['winnerPoints']).cumsum()
+    df['totalPointsLost'] = df['pointsLost'].cumsum()
     df['totalPointsLost'] = np.where(df['winnerId'] == player_id, df['totalPointsLost'].sub(df['loserPoints']), df['totalPointsWon'].sub(df['winnerPoints']))
     
     ## The outcome of our match - our y factor
@@ -40,8 +43,8 @@ def build_dataframe(player_id, player_data, opponent_id):
     df['h2hMatchesWon'] = df.groupby('opponent')['wonMatch'].cumsum().sub(df['wonMatch'])
     df['h2hMatchesLost'] = df.groupby('opponent')['lostMatch'].cumsum().sub(df['lostMatch'])
 
-    df['h2hPointsWon'] = np.where((df['winnerId'] == player_id), df.groupby('loserId')['winnerPoints'].cumsum().sub(df['winnerPoints']), df.groupby('winnerId')['loserPoints'].cumsum().sub(df['loserPoints']))
-    df['h2hPointsLost'] = np.where((df['winnerId'] == player_id), df.groupby('loserId')['loserPoints'].cumsum().sub(df['loserPoints']), df.groupby('winnerId')['winnerPoints'].cumsum().sub(df['winnerPoints']))
+    df['h2hPointsWon'] = df.groupby('opponent')['pointsWon'].cumsum().sub(df['pointsWon'])
+    df['h2hPointsLost'] = df.groupby('opponent')['pointsLost'].cumsum().sub(df['pointsLost'])
 
     # simplify independent variables
     df['totalMatchWinRatio'] = df['totalMatchesWon'] / (df['totalMatchesWon'] + df['totalMatchesLost'])
